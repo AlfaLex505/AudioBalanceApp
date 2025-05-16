d0951268a1cd9c6eaae15358e3d86e79cbdc23db
Alexis Alfaro <alexisalfaro505@gmail.com>
2025-05-16 16:19:16 -0600

    Add the file where the UI will be designed

---
+++ app/src/main/java/com/example/audiobalanceapp/ui/theme/MainScreen.kt
@@ -0,0 +1,14 @@
+package com.example.audiobalanceapp.ui.theme
+
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen(){
+
+    Button(onClick = {
+//        TODO the actions
+    }) {
+
+    }
+}

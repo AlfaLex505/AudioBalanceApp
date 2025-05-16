4a2bc8a95fd582644ad3fdea9b53994177ff3c8e
Alexis Alfaro <alexisalfaro505@gmail.com>
2025-05-16 16:35:29 -0600

    Add a column as a layout so I can add buttons

--- app/src/main/java/com/example/audiobalanceapp/ui/theme/MainScreen.kt
+++ app/src/main/java/com/example/audiobalanceapp/ui/theme/MainScreen.kt
@@ -1,14 +1,37 @@
 package com.example.audiobalanceapp.ui.theme
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun MainScreen(){
 
-    Button(onClick = {
+    Column {
+
+        Button(
+            onClick = {
 //        TODO the actions
-    }) {
+            }) {
+            Text(text = "Button 1")
+        }
+
+        Button(
+            onClick = {
+//        TODO the actions
+            }) {
+            Text(text = "Button 2")
+        }
+
+        Button(
+            onClick = {
+//        TODO the actions
+            }) {
+            Text(text = "Button 3")
+        }
 
     }
+
+
 }

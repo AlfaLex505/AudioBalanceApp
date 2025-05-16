15e60e3ece533bd260537cdce4347f0fb50d149a
Alexis Alfaro <alexisalfaro505@gmail.com>
2025-05-16 16:43:58 -0600

    Aligning the buttons

--- app/src/main/java/com/example/audiobalanceapp/ui/theme/MainScreen.kt
+++ app/src/main/java/com/example/audiobalanceapp/ui/theme/MainScreen.kt
@@ -1,14 +1,19 @@
 package com.example.audiobalanceapp.ui.theme
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 
 @Composable
 fun MainScreen(){
 
-    Column {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ){
 
         Button(
             onClick = {
